b9ec8b6b8ac0c76e026e916b167d99e0bdf08749
kabir <makkobir79@gmail.com>
2022-04-25 22:17:41 +0600

    complited

---
+++ __steps.js
@@ -0,0 +1,25 @@
+/**
+ * ------------------
+ * ONE TIME PROCESS
+ * ------------------
+ * 1. create and account.
+ * 2. verify your email.
+ * 3. install heroku cli
+ * 4. heroku login
+ * --------------------
+ * EACH PROJECT PROCESS
+ * --------------------
+ * 1. Open your project via cmd
+ * 2. heroku create
+ * 3. git add. git commit, git push
+ * 4. git push heroku main
+ * 5. go to heroku dashboard > current project> setting> reveal config vars
+ * 6. copy past .env file
+ * 7. make sure you have whitelisted all address to access mongodb
+ *  -------------------------
+ *  UPDATE SERVER WITH NEW CHANGES
+ * ----------------------------------
+ * 1. Make changes
+ * 2. git add. git commit, git push
+ * 3. git push heroku main
+ */
---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://dbUser1:EnR8ay9i7DuYPBlQ@cluster0.5kvdr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("geniusCar").collection("service");
+    const orderCollection = client.db("geniusCar").collection("order");
+
+    // AUTH
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
+    // SERVICES
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    //DELETE
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    //ORDER COLLECTION API
+
+    app.get("/order", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      const query = { email: email };
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.get("/heroku", (req, res) => {
+  res.send("server is running on heroku");
+});
+
+//listening port
+app.listen(port, () => {
+  console.log("listening", port);
+});
